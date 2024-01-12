4a0d2731f8a4e09b218152e9603d79cfb65f496d
Tobias Steinhagen <to.steinhagen@gmail.com>
2024-01-12 14:49:24 +0100

    refactoring the searchbar component

--- components/Searchbar/index.js
+++ components/Searchbar/index.js
@@ -1,61 +1,47 @@
-import useSWR from "swr"
-import {useState} from "react"
+import { useState } from "react";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const locationData = [
+  { city: "Hamburg", countryCode: "DE" },
+  { city: "Los Angeles", countryCode: "US" },
+];
+export default function Searchbar({ setLocation }) {
+  const [suggestions, setSuggestions] = useState([]);
 
-const locationData =     [
-    { city: "Hamburg", countryCode: "DE"},
-    { city: "Los Angeles", countryCode: "US"},
-  ]
-
-  function getApiURL(location){
-    if(!location){
-      return null
+  function handleChange(event) {
+    if (event.target.value.length >= 2) {
+      const filteredLocationData = locationData.filter((locationData) =>
+        locationData.city
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase())
+      );
+      setSuggestions(filteredLocationData);
+    } else {
+      setSuggestions([]);
     }
-    return `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=04d2f3433d4c4c0f64f0aeb781007f95`
   }
 
-  export default function Searchbar(){
-    const [location, setLocation] = useState("")
-    const [suggestions, setSuggestions] = useState([]);
+  function handleSubmit(event) {
+    event.preventDefault();
+    setLocation(event.target.search.value);
+  }
 
-    const { data, error, isLoading } = useSWR(getApiURL(location), fetcher); 
-
-    if (error) return <p>Error loading the data</p> 
-    if (isLoading) return <p>Loading...</p>
-
-    function handleChange(event){
-        if(event.target.value.length >= 2) {
-          const filteredLocationData = locationData.filter((locationData) => locationData.city.toLowerCase().includes(event.target.value.toLowerCase())
-          );
-          setSuggestions(filteredLocationData);        
-        } else {
-          setSuggestions([]);
-        }
-      }
-      
-      function handleSubmit(event){
-        event.preventDefault()
-        setLocation(event.target.search.value)
-      }
-
-    return(
-      <>
-        <form onSubmit={handleSubmit}>
-            <input
-            type="search"
-            list="suggestions"
-            placeholder="Search your city..."
-            onChange={handleChange}
-            name="search"
-            />
-            <datalist id="suggestions">
-            {suggestions.map((suggestion) => (
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          list="suggestions"
+          placeholder="Search your city..."
+          onChange={handleChange}
+          name="search"
+        />
+        <datalist id="suggestions">
+          {suggestions.map((suggestion) => (
             <option key={suggestion.city} value={suggestion.city}></option>
-            ))}
-            </datalist>
-            <button type="submit">Search</button>
-        </form>
-      </>
-    )
+          ))}
+        </datalist>
+        <button type="submit">Search</button>
+      </form>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
-import Searchbar from "@/components/Searchbar"
+import Searchbar from "@/components/Searchbar";
+
+import useSWR from "swr";
+import { useState } from "react";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+function getApiURL(location) {
+  if (!location) {
+    return null;
+  }
+  return `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=04d2f3433d4c4c0f64f0aeb781007f95`;
+}
 
 export default function HomePage() {
-  
-return (
-  <>
-    <Searchbar/>
-  </>
+  const [location, setLocation] = useState("");
+  console.log(location);
+
+  const { data, error, isLoading } = useSWR(getApiURL(location), fetcher);
+  console.log(data);
+
+  if (error) return <p>Error loading the data</p>;
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <Searchbar setLocation={setLocation} />
+      {!data
+        ? `Please search for your city`
+        : `The current temperature in ${location} is ${data.main.temp}°C.`}
+    </>
   );
 }
